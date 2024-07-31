7c7e7c45857218dfc0d63194ee1ac2c6d636414b
YuYing <yuying.liang00@gmail.com>
2024-07-30 22:01:22 -0400

    feat: added solution to 2015 day 3

--- 2015/day3/part1.cpp
+++ 2015/day3/part1.cpp
@@ -1,8 +1,6 @@
 #include <string>
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <cmath>
+#include <set>
 
 using namespace std;
 
@@ -16,7 +14,36 @@ int main(int argc, char *argv[])
 
   string input = argv[1];
 
-  cout << "Santa visited " << "" << " houses." << endl;
+  pair<int, int> current_location = make_pair(0, 0); // x (+east, -west), y (+north, -south)
+  set<pair<int, int>> visited_locations;
+
+  visited_locations.insert(current_location);
+  for (int i = 0; i < input.length(); i++)
+  {
+    char direction = input[i];
+    switch (direction)
+    {
+    case '^':
+      current_location.second += 1;
+      break;
+    case '>':
+      current_location.first += 1;
+      break;
+    case 'v':
+      current_location.second -= 1;
+      break;
+    case '<':
+      current_location.first -= 1;
+      break;
+    default:
+      cerr << "Input has unkown character " + direction << endl;
+      return -1;
+    }
+
+    visited_locations.insert(current_location);
+  }
+
+  cout << "Santa visited " + to_string(visited_locations.size()) + " houses." << endl;
 
   return 0;
 }
--- 2015/day3/part2.cpp
+++ 2015/day3/part2.cpp
@@ -1,8 +1,6 @@
 #include <string>
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <cmath>
+#include <set>
 
 using namespace std;
 
@@ -16,7 +14,50 @@ int main(int argc, char *argv[])
 
   string input = argv[1];
 
-  cout << "Santa visited " << "" << " houses." << endl;
+  pair<int, int> santa_location = make_pair(0, 0); // x (+east, -west), y (+north, -south)
+  pair<int, int> robo_santa_location = make_pair(0, 0);
+
+  set<pair<int, int>> visited_locations;
+
+  visited_locations.insert(santa_location);
+  visited_locations.insert(robo_santa_location);
+
+  for (int i = 0; i < input.length(); i++)
+  {
+    char direction = input[i];
+    pair<int, int> *location;
+    if (i % 2 == 0)
+    {
+      location = &santa_location;
+    }
+    else
+    {
+      location = &robo_santa_location;
+    }
+
+    switch (direction)
+    {
+    case '^':
+      (*location).second += 1;
+      break;
+    case '>':
+      (*location).first += 1;
+      break;
+    case 'v':
+      (*location).second -= 1;
+      break;
+    case '<':
+      (*location).first -= 1;
+      break;
+    default:
+      cerr << "Input has unkown character " + direction << endl;
+      return -1;
+    }
+
+    visited_locations.insert((*location));
+  }
+
+  cout << "Santa visited " + to_string(visited_locations.size()) + " houses." << endl;
 
   return 0;
 }
